b68834e5f30ce0ffdff3a4edde8a72c5c1239f4c
Vincent Fliniaux <vincent.fliniaux@gmail.com>
2023-05-24 19:32:05 -0400

    Update VF - ITEM - Advanced items repositioning.lua

--- ITEM/VF - ITEM - Advanced items repositioning.lua
+++ ITEM/VF - ITEM - Advanced items repositioning.lua
@@ -1,31 +1,31 @@
 --[[
- * ReaScript Name: Advanced items repositioning
- * Screenshot: https://imgur.com/vI4pc5B
- * Author: VF
- * Author URI: www.bruits.studio
- * Links: https://github.com/Infrabass/Reascripts
- * Version: 1.0
- * About: 
- *	# Advanced items repositioning
- *
- *	- Use start or end of items to reposition
- *	- 3 modes:
- *	    - Track
- *	    - Queue
- *	    - Timeline
- *	- Set interval between items in seconds, frames or beats
- *	- Optinally add time offset between groups of items with the same name
- *	- Non-linear factor to create rallentendo & acceleratendo
- *	- Toggle to preserve overlapping items
- *	- Toggle to preserve adjacent items
- *	- Realtime mode to tweak parameters and see result in realtime
- *	- Support moving envelope points with items
- *	- Support NVK folder items
- *	- Support ripple editing
- *	- Option to save last used settings
- *
- *	# Dependencies
- * 	- Requires ReaImGui
+ ReaScript Name: Advanced items repositioning
+ Screenshot: https://imgur.com/vI4pc5B
+ Author: VF
+ Author URI: www.bruits.studio
+ Links: https://github.com/Infrabass/Reascripts
+ Version: 1.0
+ About: 
+	# Advanced items repositioning
+
+	- Use start or end of items to reposition
+	- 3 modes:
+	    - Track
+	    - Queue
+	    - Timeline
+	- Set interval between items in seconds, frames or beats
+	- Optinally add time offset between groups of items with the same name
+	- Non-linear factor to create rallentendo & acceleratendo
+	- Toggle to preserve overlapping items
+	- Toggle to preserve adjacent items
+	- Realtime mode to tweak parameters and see result in realtime
+	- Support moving envelope points with items
+	- Support NVK folder items
+	- Support ripple editing
+	- Option to save last used settings
+
+	# Dependencies
+ 	- Requires ReaImGui
 ]]
 
 --[[
